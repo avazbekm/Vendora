82a7479117e7a357f11230e6d5dd80e22b5886c5
muqimjon <muqimjon.mr@gmail.com>
2025-05-06 00:56:32 +0500

    📱 Add MAUI project

---
+++ src/presentation/Vendora.Maui/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace Vendora.Maui;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+    }
+
+    protected override Window CreateWindow(IActivationState? activationState)
+    {
+        return new Window(new AppShell());
+    }
+}
---
+++ src/presentation/Vendora.Maui/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Vendora.Maui;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
